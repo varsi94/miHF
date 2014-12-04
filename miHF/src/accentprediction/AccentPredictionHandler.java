f5d65c776e7704040ae869a019b73e9bded23384
varsi94 <varsi.marci@gmail.com>
2014-12-04 20:04:39 +0100

    Egy eclipses kommit

--- miHF/src/accentprediction/AccentPredictionHandler.java
+++
@@ -1,7 +0,0 @@
-package accentprediction;
-
-public interface AccentPredictionHandler {
-    public String getAccentsString(String in);
-    public String getNoAccentsString(String in);
-    public Statistics getStatistics();
-}
